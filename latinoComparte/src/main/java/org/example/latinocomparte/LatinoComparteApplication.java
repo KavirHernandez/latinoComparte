f5b751be48b4e9499290ef6fcd596e8ec73d487c
Kavir <johankavirhernandez@gmail.com>
2026-05-07 19:15:49 -0500

    Initial commit

---
+++ latinoComparte/src/main/java/org/example/latinocomparte/LatinoComparteApplication.java
@@ -0,0 +1,13 @@
+package org.example.latinocomparte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatinoComparteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatinoComparteApplication.class, args);
+    }
+
+}
