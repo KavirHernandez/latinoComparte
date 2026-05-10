d7b7b6de193eea957fe1eeee54a8dafd4ed10848
JuanJ <juanitorinconm.angeles@gmail.com>
2026-05-09 23:41:44 -0500

    Todo lo que se necesita de Country

--- latinoComparte/src/main/java/org/example/latinocomparte/models/daos/CountryDao.java
+++ latinoComparte/src/main/java/org/example/latinocomparte/models/daos/CountryDao.java
@@ -1,4 +1,8 @@
 package org.example.latinocomparte.models.daos;
 
-public interface CountryDao {
+import org.example.latinocomparte.entities.CountryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryDao extends JpaRepository<CountryEntity, Long> {
+
 }
--- latinoComparte/src/main/java/org/example/latinocomparte/models/servicies/CountryService.java
+++ latinoComparte/src/main/java/org/example/latinocomparte/models/servicies/CountryService.java
@@ -1,4 +1,9 @@
 package org.example.latinocomparte.models.servicies;
 
+import org.example.latinocomparte.entities.CountryEntity;
+
+
 public interface CountryService {
+    public CountryEntity findById(Long id);
+    public void save(CountryEntity country);
 }
