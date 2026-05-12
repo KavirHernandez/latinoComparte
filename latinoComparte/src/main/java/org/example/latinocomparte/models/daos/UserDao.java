2bedf28924e90482da6bbb19be6ade209713e03f
Daniel Alejandro Torres Corredor <alejosst22@gmail.com>
2026-05-12 09:40:45 -0500

    inicio

--- latinoComparte/src/main/java/org/example/latinocomparte/models/daos/UserDao.java
+++ latinoComparte/src/main/java/org/example/latinocomparte/models/daos/UserDao.java
@@ -1,4 +1,23 @@
 package org.example.latinocomparte.models.daos;
 
-public interface UserDao {
+import org.example.latinocomparte.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    List<UserEntity> findByNombreContainingIgnoreCase(String nombre);
+
+    List<UserEntity> findByTelefonoContainingIgnoreCase(String telefono);
+
+    List<UserEntity> findByEstadoUsu(boolean estadoUsu);
+
+    List<UserEntity> findByRolIdRol(Long idRol);
+
+    List<UserEntity> findByCountryIdCountry(Long idCountry);
+
 }
---
+++ latinoComparte/src/main/java/org/example/latinocomparte/models/servicies/UserServiceImpl.java
@@ -0,0 +1,66 @@
+package org.example.latinocomparte.models.servicies;
+
+import org.example.latinocomparte.entities.UserEntity;
+import org.example.latinocomparte.models.daos.UserDao;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<UserEntity> listAll() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public UserEntity findById(Long id) {
+        return userDao.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @Override
+    public void save(UserEntity user) {
+        userDao.save(user);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userDao.deleteById(id);
+    }
+
+    @Override
+    public Optional<UserEntity> findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+
+    @Override
+    public List<UserEntity> searchByNombre(String nombre) {
+        return userDao.findByNombreContainingIgnoreCase(nombre);
+    }
+
+    @Override
+    public List<UserEntity> searchByTelefono(String telefono) {
+        return userDao.findByTelefonoContainingIgnoreCase(telefono);
+    }
+
+    @Override
+    public List<UserEntity> findByEstadoUsu(boolean estadoUsu) {
+        return userDao.findByEstadoUsu(estadoUsu);
+    }
+
+    @Override
+    public List<UserEntity> findByRol(Long idRol) {
+        return userDao.findByRolIdRol(idRol);
+    }
+
+    @Override
+    public List<UserEntity> findByCountry(Long idCountry) {
+        return userDao.findByCountryIdCountry(idCountry);
+    }
+}
